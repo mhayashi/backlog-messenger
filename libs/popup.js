fa265dfbfc2ff7ca97ffdcfc610533231f0eed0c
縣 俊貴 <agata@agata-4.local>
2010-10-09 15:24:27 +0900

    added popup js/css

---
+++ libs/popup.js
@@ -0,0 +1,109 @@
+$(function() {
+    // TODO dummyデータ
+    var data = [
+        {
+            created_user: 'agata',
+            created_on: '20050606115644',
+            content: 'コメントです1',
+            updated_on: '20050606115644'
+        },
+        {
+            created_user: 'hayashi',
+            created_on: '20050606115644',
+            content: 'コメントです2',
+            updated_on: '20050606115644'
+        }
+    ];
+    var adddata = [
+        {
+            created_user: 'agata',
+            created_on: '20050606115644',
+            content: 'コメントです3',
+            updated_on: '20050606115644'
+        },
+        {
+            created_user: 'hayashi',
+            created_on: '20050606115644',
+            content: 'コメントです4',
+            updated_on: '20050606115644'
+        }
+    ];
+    
+    var model = {
+        comments: data,
+        setComment: function(comments) {
+            this.comments = comments;
+            view.update();
+        },
+        addComments: function(comments) {
+            this.comments.concat(comments);
+            view.update(comments);
+        }
+    };
+    
+    var view = {
+        $comments: $('#comments'),
+        update: function(comments) {
+            if (comments) {
+                this.appendComments(comments);            
+            } else {
+                view.$comments.html('');
+                this.appendComments(model.comments);
+            }
+        },
+        appendComments: function(comments) {
+            $.each(comments, function(i, comment) {
+                var $c = view.createCommentElement(comment);
+                $c.hide();
+                view.$comments.prepend($c);
+                $c.animate(
+                    {height: "toggle", opacity: "toggle"},
+                    {duration: "fast", easing: "linear"}
+                );
+            });        
+        },
+        createCommentElement: function(comment) {
+            return $('<div>')
+            .append(
+                $('<img>')
+                .attr('class', 'comment_icon')
+                .attr('src', 'https://www.backlog.jp/originalicons/icons/gif/backlog_icon_51.gif')
+            )
+            .append(
+                $('<div>')
+                .attr('class', 'comment_header')
+                .append(
+                    $('<span>')
+                    .attr('class', 'comment_created_user')
+                    .text(comment.created_user)
+                )
+            )
+            .append(
+                $('<div>')
+                .attr('class', 'comment_content')
+                .html(comment.content)
+            )
+            .append(
+                $('<div>')
+                .attr('class', 'comment_footer')
+                .append(
+                    $('<span>')
+                    .attr('class', 'comment_updated_on')
+                    .text(comment.updated_on.replace(/(\d{4})(\d{2})(\d{2})(\d{2})(\d{2})(\d{2})/, "$1-$2-$3 $4:$5:$6"))
+                )
+            );
+        }
+    };
+    
+    var controller = {
+        init: function() {
+            // TODO テスト用
+            model.setComment(data);
+            $('#add').click(function() {
+                model.addComments(adddata);
+            });
+        }
+    };
+    
+    controller.init();
+});
